6e1ad1e98ad350b93e172b5ccece7c39793116bc
Vusal <Mustafayev.V@hotmail.com>
2019-06-22 11:17:09 +0400

    Initial commit.

---
+++ 2D Array - DS/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace _2D_Array___DS
+{
+    class Program
+    {
+        static int hourglassSum(int[][] arr)
+        {
+            int length = arr.Length;
+            var array = new int[3][];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[]
+            }
+            var a = arr.Rank;
+
+            return arr[0][2];
+        }
+
+        static void Main(string[] args)
+        {
+           // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int result = hourglassSum(arr);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+
+}
